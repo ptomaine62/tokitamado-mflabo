5cc84f9691fb92cfed10692c8d71abb0ff421dcd
ptomaine62 <gargoylejm@gmail.com>
2026-06-04 13:15:05 +0900

    Create sw.js

---
+++ rearena-pocket/dice-charge-battle/sw.js
@@ -0,0 +1,59 @@
+"use strict";
+
+const CACHE_NAME = "dice-charge-battle-v0-1-0";
+const CACHE_FILES = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.webmanifest",
+  "./assets/logo.svg",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(CACHE_FILES);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  const request = event.request;
+
+  if (request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(request).then((cached) => {
+      if (cached) {
+        return cached;
+      }
+
+      return fetch(request).then((response) => {
+        const copy = response.clone();
+
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(request, copy);
+        });
+
+        return response;
+      });
+    })
+  );
+});
