6ba6d183b2f326d943d2942cc2c26bc9908b1e4f
ptomaine62 <gargoylejm@gmail.com>
2026-06-04 14:46:01 +0900

    Update sw.js

--- rearena-pocket/dice-charge-battle/sw.js
+++ rearena-pocket/dice-charge-battle/sw.js
@@ -1,63 +1,21 @@
 "use strict";
 
-const CACHE_NAME = "dice-charge-battle-v0-1-2";
-const CACHE_FILES = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./app.js",
-  "./manifest.webmanifest",
-  "./assets/logo.svg",
-];
-
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(CACHE_FILES);
-    })
-  );
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys
-          .filter((key) => key !== CACHE_NAME)
-          .map((key) => caches.delete(key))
-      );
-    })
+    Promise.all([
+      caches.keys().then((keys) => {
+        return Promise.all(keys.map((key) => caches.delete(key)));
+      }),
+      self.registration.unregister(),
+      self.clients.claim(),
+    ])
   );
-  self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
-  const request = event.request;
-
-  if (request.method !== "GET") {
-    return;
-  }
-
-  event.respondWith(
-    fetch(request)
-      .then((response) => {
-        const copy = response.clone();
-
-        caches.open(CACHE_NAME).then((cache) => {
-          cache.put(request, copy);
-        });
-
-        return response;
-      })
-      .catch(() => {
-        return caches.match(request).then((cached) => {
-          if (cached) {
-            return cached;
-          }
-
-          return caches.match("./");
-        });
-      })
-  );
+  return;
 });
