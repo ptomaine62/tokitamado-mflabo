a04cfe305f1841e7c33d7163454cf24434220305
ptomaine62 <gargoylejm@gmail.com>
2026-09-18 19:34:54 +0900

    Refresh E-STIM security cache

--- coyote-remote/sw.js
+++ coyote-remote/sw.js
@@ -1,4 +1,4 @@
-const CACHE='estim-remote-v0.2.7-security2';
+const CACHE='estim-remote-v0.2.7-security3';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./vendor/peerjs.min.js','./turn-config.js'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil((async()=>{for(const k of await caches.keys())if(k!==CACHE)await caches.delete(k);await self.clients.claim()})()));
