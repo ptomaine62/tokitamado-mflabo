75d126e333ec7ee9e0b51959cddddf0804e76fdd
ptomaine62 <gargoylejm@gmail.com>
2026-09-18 19:33:39 +0900

    Activate E-STIM security build

--- coyote-remote/sw.js
+++ coyote-remote/sw.js
@@ -1,5 +1,5 @@
-const CACHE='estim-remote-v0.2.6-test1';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./vendor/peerjs.min.js'];
+const CACHE='estim-remote-v0.2.7-security2';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./vendor/peerjs.min.js','./turn-config.js'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil((async()=>{for(const k of await caches.keys())if(k!==CACHE)await caches.delete(k);await self.clients.claim()})()));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET'||new URL(e.request.url).origin!==self.location.origin)return;e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});return r}).catch(()=>caches.match(e.request)))});
