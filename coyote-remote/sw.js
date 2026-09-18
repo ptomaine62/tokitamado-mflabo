02ed36830432f7893058dea4f0116ac9f53d0698
ptomaine62 <gargoylejm@gmail.com>
2026-09-18 09:15:33 +0900

    Refresh E-STIM WAN cache for v0.2.1

--- coyote-remote/sw.js
+++ coyote-remote/sw.js
@@ -1,4 +1,4 @@
-const CACHE='estim-remote-v0.2.0-wan1';
+const CACHE='estim-remote-v0.2.1-wan2';
 const ASSETS=["./","./index.html","./manifest.webmanifest","./bundle/p01.txt","./bundle/p02.txt","./bundle/p03.txt","./bundle/p04.txt","./bundle/p05.txt","./bundle/p06.txt","./bundle/p07.txt","./bundle/p08.txt","./bundle/p09.txt","./bundle/p10.txt","./bundle/p11.txt","./bundle/p12.txt","./bundle/p13.txt","./bundle/p14.txt","./bundle/p15.txt","./bundle/p16.txt","./bundle/p17.txt","./bundle/p18.txt","./bundle/p19.txt","./bundle/p20.txt","./bundle/p21.txt","./bundle/p22.txt","./bundle/p23.txt","./bundle/p24.txt","./bundle/p25.txt","./bundle/p26.txt","./bundle/p27.txt","./bundle/p28.txt","./bundle/p29.txt","./bundle/p30.txt"];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil((async()=>{for(const k of await caches.keys())if(k!==CACHE)await caches.delete(k);await self.clients.claim()})()));
